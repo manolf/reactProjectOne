4141ccbba9e58420e29cade214b9b8708ae8b34b
Manu Thamer <yanceen@yahoo.com>
2020-11-05 09:18:43 +0100

    menu stadt krems

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+//dynamic approach for getting all unique values as buttons
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
 
 function App() {
+  const [isMainpage, setIsMainpage] = useState(false);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  if (isMainpage) {
+    return (
+      <main>
+        <section className="menu section">
+          <div className="title">
+            <h2>Speisen</h2>
+
+            <div className="underline"></div>
+           
+          </div>
+          <Categories categories={categories} filterItems={filterItems} />
+          <Menu items={menuItems} />
+        </section>
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="menu section">
+      <div className="title">
+        <h2>Die neue Stadt Krems</h2>
+        <div className="underline"></div>
+        <div className="btn-container"></div>
+        <button onClick={() => setIsMainpage(true)} className="filter-btn">
+          Unsere Speisekarte
+        </button>
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,111 @@
+const menu = [
+  {
+    id: 1,
+    title: "Käsespätzle",
+    category: "Vegetarisch",
+    price: 11.99,
+    img:
+      "https://cdn.pixabay.com/photo/2014/12/24/13/01/cheese-noodles-579077_1280.jpg",
+    desc: `Die besondere Spezialiät aus Vorarlberg und Schweiz: auf Wunsch auch ohne Speckwürfel. `,
+  },
+  {
+    id: 2,
+    title: "Marillenpalatschinken",
+    category: "Etwas Süßes",
+    price: 3.99,
+    img:
+      "https://cdn.pixabay.com/photo/2017/01/30/13/56/pancakes-2020870_1280.jpg",
+    desc: `Die Wurzeln aus Ungarn, der Name aus Rumänien, der Inhalt aus der Wachau: das süße Finale, das alles abrundet. `,
+  },
+  {
+    id: 3,
+    title: "Großes Wiener",
+    category: "Fleischiges",
+    price: 18.99,
+    img:
+      "https://cdn.pixabay.com/photo/2019/03/25/20/20/schnitzel-4081269_1280.jpg",
+    desc: `Der Klassiker seit Jahrhunderten: dabei verwenden wir nur das feinste Stück des Kalbes, goldbraun gebacken, so wie es sich gehört.`,
+  },
+  {
+    id: 4,
+    title: "Blunzengröstel",
+    category: "Fleischiges",
+    price: 15.99,
+    img:
+      "https://i.pinimg.com/564x/d4/7d/ec/d47dec58040352c4e4e26ca89fd2630f.jpg",
+    desc: `die Blunzn auf Alt-Wiener Art: mit köstlichen Bratkartoffeln, Speck und Zwiebel ergänzt und Kren, Kümmel, Pfeffer, Majoran und Knoblauch verfeinert.  `,
+  },
+  {
+    id: 5,
+    title: "Salat mit gebratenen Hühnerstreifen",
+    category: "Salate",
+    price: 11.9,
+    img:
+      "https://cdn.pixabay.com/photo/2017/03/19/14/59/italian-salad-2156723_1280.jpg",
+    desc: `knusprig gebratene Hühnerstreifen auf grünem Salat und Erdäpfel `,
+  },
+  {
+    id: 6,
+    title: "Geröstete Schweinsleber mit Erdäpfel",
+    category: "Fleischiges",
+    price: 13.99,
+    img:
+      "https://img.chefkoch-cdn.de/rezepte/181041078331588/bilder/987993/crop-600x400/geroestete-leber.jpg",
+    desc: `rundum gebraten, mit Majoran gewürzt und mit Essigwasser abgelöscht: serviert mit Petersilienkartoffel oder Reis`,
+  },
+  {
+    id: 7,
+    title: "Cordon Bleu",
+    category: "Fleischiges",
+    price: 8.99,
+    img:
+      "https://cdn.pixabay.com/photo/2019/10/09/19/47/cordon-4538108_960_720.jpg",
+    desc: `nach Wunsch vom Schwein oder vom Huhn - gefüllt mit Schinken, Käse und ganz viel Liebe `,
+  },
+  {
+    id: 8,
+    title: "Mohr im Hemd",
+    category: "Etwas Süßes",
+    price: 4.99,
+    img:
+      "https://cdn.pixabay.com/photo/2019/09/24/21/49/cake-4502270_960_720.jpg",
+    desc: `Zubereitung nach Wunsch auch mit geschmolzener heißer Schokolade und Schlagobers und ohne Himbeere`,
+  },
+  {
+    id: 9,
+    title: "Krautsalat",
+    category: "Salate",
+    price: 2.6,
+    img:
+      "https://cdn.pixabay.com/photo/2020/03/28/14/53/salad-4977374_960_720.jpg",
+    desc: "",
+  },
+  {
+    id: 10,
+    title: "gemischter Salat",
+    category: "Salate",
+    price: 3.6,
+    img:
+      "https://cdn.pixabay.com/photo/2017/06/03/21/17/salad-2369806_960_720.jpg",
+    desc: "grüner Salat, Karotten, Tomaten, Gurken, Kraut",
+  },
+  {
+    id: 11,
+    title: "Knödel mit Ei",
+    category: "Vegetarisch",
+    price: 8.6,
+    img:
+      "https://www.gutekueche.at/img/rezept/4459/geroestete-knoedel-mit-ei_1496764522.jpg",
+    desc: "Eier und Knödel - einfach (und) köstlich",
+  },
+  {
+    id: 12,
+    title: "Salat mit gebratenen Hühnerstreifen",
+    category: "Salate",
+    price: 11.9,
+    img:
+      "https://www.steirische-spezialitaeten.at/wp-content/uploads/2017/08/backhendlsalat-750x500.jpg",
+    desc: `Geheimtipp unserer StammkundInnen: gebackene Hühnerstreifen auf grünem Salat und Erdäpfel `,
+  },
+];
+export default menu;
